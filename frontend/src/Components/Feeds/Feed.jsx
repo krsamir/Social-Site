6df04a65c2d616f3df4601fd28be481cb33d63f9
Samir Kumar <samir1741012321@gmail.com>
2021-07-11 22:53:38 +0530

    Adding fonts

--- frontend/src/Components/Feeds/Feed.jsx
+++ frontend/src/Components/Feeds/Feed.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { Avatar } from "@material-ui/core";
 import "./Feed.css";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import Image from "react-bootstrap/Image";
 const Feed = ({ data }) => {
+  const [index, setIndex] = useState(0);
   if (data) {
     var fullName = "";
     if (data.posted_by.split(" ").length >= 2) {
@@ -25,24 +29,67 @@ const Feed = ({ data }) => {
             <div className="feed__hrline"></div>
             <div className="feed__content">
               <div className="feed__text">{data.text}</div>
-              <div className="feed__image">
-                {media !== null &&
-                  media.map((value, index) => {
-                    return (
-                      <div key={index}>
-                        <img
-                          // src={`../../Uploads/${value.filename}`}
-                          src={
-                            require(`../../Uploads/${value.filename}`).default
+              {media !== null && (
+                <div className="feed__image feed__post__imageBox">
+                  <div className="bigBox" style={{ width: "100%" }}>
+                    <div className="box1">
+                      <ChevronLeftIcon
+                        className="arrow"
+                        onClick={() => {
+                          if (index >= 1) {
+                            setIndex((prevState) => prevState - 1);
                           }
-                          alt="imageHere"
-                          height="100px"
-                          width="100px"
-                        />
-                      </div>
-                    );
-                  })}
-              </div>
+                          console.log(data.media.length, index);
+                        }}
+                      />
+                    </div>
+                    <div className="box2">
+                      <Image
+                        className="feed__post__images"
+                        // src={`../../Uploads/${value.filename}`}
+                        src={
+                          require(`../../Uploads/${media[index].filename}`)
+                            .default
+                        }
+                        alt="imageHere"
+                        // height="100px"
+                        // width="auto"
+                        fluid
+                      />
+
+                      {/* {media !== null &&
+                      media.map((value, index) => {
+                        return (
+                          <Image
+                            key={index}
+                            className="feed__post__images"
+                            // src={`../../Uploads/${value.filename}`}
+                            src={
+                              require(`../../Uploads/${value.filename}`).default
+                            }
+                            alt="imageHere"
+                            // height="100px"
+                            // width="auto"
+                            fluid
+                          />
+                        );
+                      })} */}
+                    </div>
+                    <div className="box3">
+                      <ChevronRightIcon
+                        className="arrow"
+                        onClick={() => {
+                          if (index < data.media.length - 1) {
+                            setIndex((prevState) => prevState + 1);
+                          }
+                          console.log(data.media.length, index);
+                        }}
+                      />
+                    </div>
+                  </div>
+                  <div className="feed__hrline"></div>
+                </div>
+              )}
             </div>
           </div>
         </div>
